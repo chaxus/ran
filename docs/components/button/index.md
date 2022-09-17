448ee659f7aacbed11fab7f9e881a14a986c09cc
chaxus <nouo18@163.com>
2022-09-17 20:48:26 +0800

    feat: 按钮组件完成

--- docs/components/button/index.md
+++ docs/components/button/index.md
@@ -6,7 +6,7 @@
 基础的函数用法
 
 <div style="margin-bottom:20px;">
-    <xu-button type="primary"><span slot="btn-content">主要按钮</span></xu-button>
-    <xu-button type="warning"><span slot="btn-content">警告按钮</span></xu-button>
-    <xu-button type="default"><span slot="btn-content">默认按钮</span></xu-button>
+    <xu-button type="primary"><span slot="r-btn_content">主要按钮</span></xu-button>
+    <xu-button type="warning"><span slot="r-btn_content">警告按钮</span></xu-button>
+    <xu-button type="default"><span slot="r-btn_content">默认按钮</span></xu-button>
 </div>
--- src/button.ts
+++ src/button.ts
@@ -2,56 +2,59 @@ function RButton() {
   const template = document.createElement("template");
   const slot = document.createElement('slot')
   const btn = document.createElement('div');
-  btn.setAttribute('class', 'xu-button');
-  slot.setAttribute('name', 'btn-content')
+  btn.setAttribute('class', 'r-btn');
+  slot.setAttribute('name', 'r-btn_content')
   btn.appendChild(slot)
   template.appendChild(btn)
   class Button extends HTMLElement {
+    _type: Record<string,string>;
+    _btn: Element;
     constructor() {
       super();
-      // 获取模板内容
-      // const templateContent = template.content;
-      // 组件内容
-      const shadowRoot = this.attachShadow({ mode: 'closed' });
-      // const btn = document.createElement('button');
-      // 克隆一份 防止重复使用 污染
-      // btn.appendChild(templateContent.cloneNode(true));
-      // btn.setAttribute('class', 'xu-button');
-      // 定义并获取按钮类型  primary | warning | default
-      const type: Record<string, string> = {
-        'primary': '#06c',
-        'warning': 'red',
+      this._type = {
+        'primary': 'r-btn_primary',
+        'warning': 'r-btn_warning',
         'default': '#f0f0f0'
       }
-      const attribute = this.getAttribute('type') || ''
-      console.log('attribute--->',this,this.getAttribute('type'))
-      const btnType = Object.keys(type).includes(attribute) ? attribute : 'default';
-      const btnColor = btnType === 'default' ? '#888' : '#fff';
-
+      this._btn = btn.cloneNode(true) as Element
+      const shadowRoot = this.attachShadow({ mode: 'closed' });
       // 创建样式
       const style = document.createElement('style');
       // 为shadow Dom添加样式
       style.textContent = `
-        .xu-button {
+        .r-btn {
           position: relative;
           margin-right: 3px;
           display: inline-block;
           padding: 6px 20px;
           border-radius: 30px;
-          background-color: ${type[btnType]};
-          color: ${btnColor};
+          background-color: #f0f0f0;
+          color: #888;
           outline: none;
           border: none;
           box-shadow: inset 0 5px 10px rgba(0,0,0, .3);
           cursor: pointer;
         }
+        .r-btn_primary {
+          background-color: #06c;
+          color: #fff;
+        }
+        .r-btn_warning {
+          background-color: red;
+          color: #fff;
+        }
       `
       shadowRoot.appendChild(style);
-      shadowRoot.appendChild(btn.cloneNode(true));
+      shadowRoot.appendChild(this._btn);
+    }
+    connectedCallback(){
+      const type = this.getAttribute('type');
+      if (type) {
+        this._btn.className = `r-btn ${this._type[type]}`
+      }
     }
   }
   window.customElements.define('xu-button', Button);
-
 }
 
 export default RButton()
