7deef203ec5c99fe51b299f39957214710a3c35e
chaxus <nouo18@163.com>
2022-09-25 20:10:06 +0800

    feat: 遗留打包库函数问题

--- packages/ranuts/vite.config.ts
+++ packages/ranuts/vite.config.ts
@@ -3,7 +3,6 @@ import path, { resolve } from "path";
 import { fileURLToPath } from "url";
 import dts from 'vite-plugin-dts'
 
-
 const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = path.dirname(__filename);
@@ -13,12 +12,12 @@ export default defineConfig({
     minify: 'terser',
     sourcemap: true, // 输出单独 source文件
     lib: {
-        entry: "./index.ts",
-        name: "ranuts",
-        fileName: "index",
-        // 导出模块格式
-        formats: ["es", "umd"],
-      },
+      entry: "./index.ts",
+      name: "ranuts",
+      fileName: "index",
+      // 导出模块格式
+      formats: ["es", "umd"],
+    },
   },
   resolve: {
     alias: {
@@ -26,7 +25,7 @@ export default defineConfig({
     },
     extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json"],
   },
-  plugins:[
+  plugins: [
     dts({
       tsConfigFilePath: './tsconfig.json'
     }),
