f0983781d11b6fed7b0d948552fb4d27b535a6b3
chaxus <nouo18@163.com>
2022-09-25 10:45:22 +0800

    feat: 增加less样式处理

--- packages/docs/.vitepress/theme/index.ts
+++ packages/docs/.vitepress/theme/index.ts
@@ -1,6 +1,6 @@
 import DefaultTheme from 'vitepress/theme'
-// import '../../../ranui/index' // 开发
-import 'ranui' // 打包
+import '../../../ranui/index' // 开发
+// import 'ranui' // 打包
 
 export default {
   ...DefaultTheme,
--- packages/ranui/components/button/index.ts
+++ packages/ranui/components/button/index.ts
@@ -1,3 +1,5 @@
+import styles from './index.less'
+
 function RButton() {
   const template = document.createElement("template");
   const slot = document.createElement('slot')
@@ -21,58 +23,7 @@ function RButton() {
       // 创建样式
       const style = document.createElement('style');
       // 为shadow Dom添加样式
-      style.textContent = `
-        .r-btn {
-          position: relative;
-          display: inline-block;
-          font-weight: 400;
-          white-space: nowrap;
-          text-align: center;
-          background-image: none;
-          border: 1px solid transparent;
-          box-shadow: 0 2px #00000004;
-          cursor: pointer;
-          transition: all .3s cubic-bezier(.645,.045,.355,1);
-          -webkit-user-select: none;
-          -moz-user-select: none;
-          -ms-user-select: none;
-          user-select: none;
-          touch-action: manipulation;
-          height: 22px;
-          line-height: 22px;
-          padding: 4px 15px;
-          font-size: 14px;
-          border-radius: 2px;
-          color: #000000d9;
-          border-color: #d9d9d9;
-          background: #fff
-        }
-        .r-btn,.r-btn:active,.r-btn:focus {
-          outline: 0
-        }
-        .r-btn_primary {
-          border-color: #1890ff;
-          background-color: #1890ff;
-          color: #fff;
-        }
-        .r-btn_warning {
-          border-color: #ff4d4f;
-          background-color: #ff4d4f;
-          color: #fff;
-        }
-        .r-btn_default:hover {
-          border-color: #1890ff;
-          color: #1890ff;
-        }
-        .r-btn_primary:hover{
-          background-color: #40a9ff;
-          color: #fff;
-        }
-        .r-btn_warning:hover {
-          background-color: #ff7875;
-          color: #fff;
-        }
-      `
+      style.textContent = styles
       shadowRoot.appendChild(style);
       shadowRoot.appendChild(this._btn);
     }
