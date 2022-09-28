b2c407e2d35406e3c3839d2cc5324e7e212af630
chaxus <nouo18@163.com>
2022-09-28 23:27:11 +0800

    feat: 更新vite配置

--- packages/ranui/vite.config.ts
+++ packages/ranui/vite.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
     }),
     componentsIndex({
       path:[
-        './components/',
+        './components',
         // resolve(__dirname, "components/")
       ]
     })
