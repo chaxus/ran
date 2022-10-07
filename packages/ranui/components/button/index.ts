f3ab9134f25f9d24daf1b652077dae611c876454
chaxus <nouo18@163.com>
2022-10-07 21:51:53 +0800

    feat: 增加按钮disabled

--- packages/ranui/components/button/index.ts
+++ packages/ranui/components/button/index.ts
@@ -1,31 +1,49 @@
 
+
 function Component() {
   const template = document.createElement("template");
   const slot = document.createElement('slot')
   const btn = document.createElement('div');
-  btn.setAttribute('class', 'r-btn r-btn_default');
   slot.setAttribute('name', 'r-btn_content')
   btn.appendChild(slot)
   template.appendChild(btn)
   class CustomElement extends HTMLElement {
-    _type: Record<string,string>;
+    static get observedAttributes() { return ['disabled'] }
     _btn: Element;
     constructor() {
       super();
-      this._type = {
-        'primary': 'r-btn_primary',
-        'warning': 'r-btn_warning',
-        'default': 'r-btn_default'
-      }
       this._btn = btn.cloneNode(true) as HTMLElement
-      const shadowRoot = this.attachShadow({ mode: "closed" });
+      const shadowRoot = this.attachShadow({ mode: "open" });
       shadowRoot.appendChild(this._btn);
     }
-    connectedCallback(){
-      const type = this.getAttribute('type');
-      if (type) {
-        this._btn.className = `r-btn ${this._type[type]}`
+    get disabled() {
+      const disable = this.getAttribute('disabled')
+      return disable
+    }
+    set disabled(value) {
+      if (!value || value === "false") {
+        this.removeAttribute('disabled');
+      } else {
+        this.setAttribute('disabled', value);
       }
+    }
+    connectedCallback() {
+      this._btn.addEventListener('mousedown',  (event:any) => {
+        if (!this.disabled) {
+          const { left, top } = this.getBoundingClientRect();
+          this.style.setProperty('--x', (event.clientX - left) + 'px');
+          this.style.setProperty('--y', (event.clientY - top) + 'px');
+        }
+      })
+    }
+    attributeChangedCallback (name:string, oldValue:string, newValue:string) {
+      if(name == 'disabled' && this._btn){
+        if(newValue){
+            this._btn.setAttribute('disabled', newValue);
+        }else{
+            this._btn.removeAttribute('disabled');
+        }
+    }
     }
   }
   window.customElements.define('r-button', CustomElement);
