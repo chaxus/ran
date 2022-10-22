8e4d87ae60fe730871af5baa685e0026b957acac
chaxus <nouo18@163.com>
2022-10-22 23:13:53 +0800

    feat: 增加希尔排序

--- packages/ranuts/library/sort/insert.ts
+++ packages/ranuts/library/sort/insert.ts
@@ -1,6 +1,10 @@
 
 
-
+/**
+ * @description: 插入排序，维护一个有序的数组，将后面的数字插入到前面的有序数组中国
+ * @param {Array} list
+ * @return {Array}
+ */
 const insert = (list: Array<string>) => {
     const { length } = list
     for (let i = 1; i < length; i++) {
--- packages/ranuts/test/sort.spec.ts
+++ packages/ranuts/test/sort.spec.ts
@@ -1,6 +1,7 @@
 import bubble from '@/sort/bubble'
 import select from '@/sort/select'
 import insert from '@/sort/insert'
+import shell from '@/sort/shell'
 import randomArray from '@/sort/randomArray'
 import { describe, expect, it } from 'vitest'
 
@@ -25,6 +26,14 @@ describe("sort", () => {
         for (let i = 0; i < 10; i++) {
             const exampleArray = randomArray()
             const result = insert(exampleArray)
+            const rightResult = [...exampleArray].sort((a: number, b: number) => a - b)
+            expect(result).toEqual(rightResult);
+        }
+    });
+    it("shell sort", () => {
+        for (let i = 0; i < 10; i++) {
+            const exampleArray = randomArray()
+            const result = shell(exampleArray)
             const rightResult = [...exampleArray].sort((a: number, b: number) => a - b)
             expect(result).toEqual(rightResult);
         }
