48c03c8e53347621dcc5c7031be4326230f418f4
chaxus <nouo18@163.com>
2022-10-23 15:54:50 +0800

    feat: 增加归并排序和快速排序

--- packages/ranuts/library/sort/quick.ts
+++ packages/ranuts/library/sort/quick.ts
@@ -1,10 +1,57 @@
+
+const swap = (list: Array<number>, left: number, right: number) => {
+    let temp = list[left]
+    list[left] = list[right]
+    list[right] = temp
+}
+/**
+ * @description: 设置基准值pivot
+ * @param {Array} list
+ * @param {number} left
+ * @param {number} right
+ * @return {number} index
+ */
+const partition = (list: Array<number>, left: number, right: number) => {
+    let pivot = left
+    let index = pivot + 1
+    for (let i = index; i <= right; i++) {
+        if (list[i] < list[pivot]) {
+            let temp = list[i]
+            list[i] = list[index]
+            list[index] = temp
+            index++
+        }
+    }
+    let temp = list[pivot]
+    list[pivot] = list[index - 1]
+    list[index - 1] = temp
+    return index - 1
+}
+/**
+ * @description: 不断分区，设置基准值
+ * @param {Array} list
+ * @param {number} left
+ * @param {number} right
+ * @return {Array}
+ */
+const combine = (list: Array<number>, left: number, right: number) => {
+    let partitionIndex;
+    if (left < right) {
+        partitionIndex = partition(list, left, right)
+        combine(list, left, partitionIndex - 1)
+        combine(list, partitionIndex + 1, right)
+    }
+    return list
+
+}
 /**
  * @description: 快速排序
  * @param {Array} list
  * @return {Array}
  */
- const quick = (list: Array<string>) => {
-    return list
+const quick = (list: Array<number>) => {
+    const { length } = list;
+    return combine(list, 0, length - 1)
 }
 
 export default quick
--- packages/ranuts/test/sort.spec.ts
+++ packages/ranuts/test/sort.spec.ts
@@ -2,40 +2,49 @@ import bubble from '@/sort/bubble'
 import select from '@/sort/select'
 import insert from '@/sort/insert'
 import shell from '@/sort/shell'
+import merge from '@/sort/merge'
+import quick from '@/sort/quick'
 import randomArray from '@/sort/randomArray'
+import readDir from '@/functions/readDir'
 import { describe, expect, it } from 'vitest'
+import path, { resolve } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const handing = (name: string, sort: Function) => {
+    it(name, () => {
+        console.time(name)
+        for (let i = 0; i < 10; i++) {
+            const exampleArray = randomArray()
+            const result = sort(exampleArray)
+            const rightResult = [...exampleArray].sort((a: number, b: number) => a - b)
+            expect(result).toEqual(rightResult);
+        }
+        console.timeEnd(name)
+    });
+}
+// const dirPath = resolve(__dirname, '../library/sort');
+
+
 
 describe("sort", () => {
-    it("bubble sort", () => {
-        for (let i = 0; i < 10; i++) {
-            const exampleArray = randomArray()
-            const result = bubble(exampleArray)
-            const rightResult = [...exampleArray].sort((a: number, b: number) => a - b)
-            expect(result).toEqual(rightResult);
-        }
-    });
-    it("select sort", () => {
-        for (let i = 0; i < 10; i++) {
-            const exampleArray = randomArray()
-            const result = select(exampleArray)
-            const rightResult = [...exampleArray].sort((a: number, b: number) => a - b)
-            expect(result).toEqual(rightResult);
-        }
-    });
-    it("insert sort", () => {
-        for (let i = 0; i < 10; i++) {
-            const exampleArray = randomArray()
-            const result = insert(exampleArray)
-            const rightResult = [...exampleArray].sort((a: number, b: number) => a - b)
-            expect(result).toEqual(rightResult);
-        }
-    });
-    it("shell sort", () => {
-        for (let i = 0; i < 10; i++) {
-            const exampleArray = randomArray()
-            const result = shell(exampleArray)
-            const rightResult = [...exampleArray].sort((a: number, b: number) => a - b)
-            expect(result).toEqual(rightResult);
-        }
-    });
+    // it('read-dir', async () => {
+    // const param = {
+    //     dirPath,
+    //     ignores: ['randomArray.ts']
+    // }
+    // const sort = await readDir(param)
+    // Object.keys(sort).forEach(key => {
+    //     console.log('key--->', key, sort[key]);
+    //     handing(`${key} sort`, sort[key])
+    // })
+    // });
+    handing("bubble sort", bubble)
+    handing("select sort", select)
+    handing("insert sort", insert)
+    handing("shell sort", shell)
+    handing("merge sort", merge)
+    handing("quick sort", quick)
 });
