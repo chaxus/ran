e79743a883531550f4f6be44055015e945d85837
chaxus <81380@163.com>
2022-10-28 19:31:40 +0800

    feat: 构建tabs组件

--- packages/ranui/components/tabs/index.ts
+++ packages/ranui/components/tabs/index.ts
@@ -1,7 +1,11 @@
-import { isDisabled } from '@/utils/index'
+import {
+  isDisabled,
+  setElementClass,
+  deleteElementChildClass,
+} from "@/utils/index";
 class Tabs extends HTMLElement {
   static get observedAttributes() {
-    return ["active", 'forceRender'];
+    return ["active", "forceRender", "type", "align"];
   }
   _container: HTMLDivElement;
   _header: HTMLDivElement;
@@ -46,7 +50,7 @@ class Tabs extends HTMLElement {
     this._container.appendChild(this._header);
     this._container.appendChild(this._content);
 
-    this.tabHeaderKeyMapIndex = {}
+    this.tabHeaderKeyMapIndex = {};
 
     const shadowRoot = this.attachShadow({ mode: "closed" });
     shadowRoot.appendChild(this._container);
@@ -60,6 +64,10 @@ class Tabs extends HTMLElement {
     this.setAttribute("align", value);
   }
 
+  set type(value) {
+    this.setAttribute("type", value);
+  }
+
   get type() {
     return this.getAttribute("type") || "flat";
   }
@@ -71,15 +79,11 @@ class Tabs extends HTMLElement {
   set active(value) {
     if (value) {
       this.setAttribute("active", value);
-      this.setTabLine(value)
-      this.setTabContent(value)
+      this.setTabLine(value);
+      this.setTabContent(value);
     } else {
       this.removeAttribute("active");
     }
-  }
-
-  set type(value) {
-    this.setAttribute("type", value);
   }
   /**
    * @description: 构建tabPane组件key值和index的映射，同时判断一个tabs下的tabPane key值不能重复
@@ -87,13 +91,15 @@ class Tabs extends HTMLElement {
    * @param {number} index
    */
   initTabHeaderKeyMapIndex = (key: string, index: number) => {
-    const value = this.tabHeaderKeyMapIndex[key]
+    const value = this.tabHeaderKeyMapIndex[key];
     if (value) {
-      throw new Error('tab 组件的 key 值存在重复, 或者某个 tab 组件缺少 key 属性')
+      throw new Error(
+        "tab 组件的 key 值存在重复, 或者某个 tab 组件缺少 key 属性"
+      );
     } else {
-      this.tabHeaderKeyMapIndex[key] = index
+      this.tabHeaderKeyMapIndex[key] = index;
     }
-  }
+  };
   /**
    * @description: 根据传入的tabPane生成tabs的头部
    * @param {Element} tabPane
@@ -101,37 +107,73 @@ class Tabs extends HTMLElement {
    * @return {Element}
    */
   createTabHeader(tabPane: Element, index: number) {
-    const label = tabPane.getAttribute("label") || '';
-    const key = tabPane.getAttribute('key') || `${index}`
-    const type = tabPane.getAttribute('type') || 'text'
-    this.initTabHeaderKeyMapIndex(key, index)
-    const tabHeader = document.createElement('r-button')
-    tabHeader.setAttribute('class', 'tab-header_nav__item')
-    tabHeader.setAttribute('type', type)
-    isDisabled(tabPane) && tabHeader.setAttribute('disabled', '')
-    tabHeader.setAttribute('ran-key', key)
-    tabHeader.innerHTML = label
-    return tabHeader
+    const label = tabPane.getAttribute("label") || "";
+    const key = tabPane.getAttribute("key") || `${index}`;
+    const type = tabPane.getAttribute("type") || "text";
+    this.initTabHeaderKeyMapIndex(key, index);
+    const tabHeader = document.createElement("r-button");
+    tabHeader.setAttribute("class", "tab-header_nav__item");
+    tabHeader.setAttribute("type", type);
+    isDisabled(tabPane) && tabHeader.setAttribute("disabled", "");
+    tabHeader.setAttribute("ran-key", key);
+    tabHeader.innerHTML = label;
+    return tabHeader;
   }
+  /**
+   * @description: 初始化tabLine的位置，主要是当tabs的align属性为center时需要处理
+   */
+  initTabLineAlignCenter = () => {
+    const { length } = this._nav.children;
+    let left = 0;
+    for (let i = 0; i < length; i++) {
+      const { width = 0 } = this._nav.children[i].getBoundingClientRect();
+      left += width;
+    }
+    this._line.style.setProperty("left", `calc(50% - ${left / 2}px)`);
+  };
+  /**
+   * @description: 初始化tabLine的位置，主要是当tabs的align属性为end时需要处理
+   */
+  initTabLineAlignEnd = () => {
+    const { length } = this._nav.children;
+    let left = 0;
+    for (let i = 0; i < length; i++) {
+      const { width = 0 } = this._nav.children[i].getBoundingClientRect();
+      left += width;
+    }
+    this._line.style.setProperty("left", `calc(100% - ${left}px)`);
+  };
   /**
    * @description: 通过key值设置tabLine的位置
    * @param {string} key
    */
   setTabLine = (key: string) => {
     if (key) {
-      const index = this.tabHeaderKeyMapIndex[key]
-      this._line.style.setProperty('transform', `translateX(${100 * index}%)`)
+      const index = this.tabHeaderKeyMapIndex[key];
+      // 计算tabHeader的宽度，给tabLine赋值
+      const TabHeader = this._nav.children[index];
+      const { width = 0 } = TabHeader.getBoundingClientRect();
+      this._line.style.setProperty("width", `${width}px`);
+      // 计算tabLine的移动距离
+      let distance = 0;
+      for (let i = 0; i < index; i++) {
+        const item = this._nav.children[i];
+        const { width = 0 } = item.getBoundingClientRect();
+        distance += width;
+      }
+      // 设置移动的距离
+      this._line.style.setProperty("transform", `translateX(${distance}px)`);
     }
-  }
+  };
   /**
    * @description: 通过传入的key值设置tabContent
    */
   setTabContent = (key: string) => {
     if (key) {
-      const index = this.tabHeaderKeyMapIndex[key]
-      this._wrap.style.setProperty('transform', `translateX(${index * -100}%)`)
+      const index = this.tabHeaderKeyMapIndex[key];
+      this._wrap.style.setProperty("transform", `translateX(${index * -100}%)`);
     }
-  }
+  };
   /**
    * @description: 根据点击设置tabLine的位置
    * @param {Event} e
@@ -139,32 +181,37 @@ class Tabs extends HTMLElement {
    * @param {number} width
    */
   clickTabHead = (e: Event) => {
-    const tabHeader = e.target as Element
-    const key = tabHeader.getAttribute('ran-key')
-    const disabled = isDisabled(tabHeader)
+    const tabHeader = e.target as Element;
+    const key = tabHeader.getAttribute("ran-key");
+    const disabled = isDisabled(tabHeader);
     if (!disabled && key) {
-      this.setAttribute('active', key)
-      this.setTabLine(key)
-      this.setTabContent(key)
+      this.setAttribute("active", key);
+      this.setTabLine(key);
+      this.setTabContent(key);
+      deleteElementChildClass(this._nav, "active");
+      setElementClass(tabHeader, "active");
     }
-  }
+  };
   /**
    * @description: 初始化tabs的active属性和tabLine,tabContent
    */
   initActive = () => {
-    const tabHeaderList = [...this._nav.children]
-    const initTabHeader = tabHeaderList.filter(item => !isDisabled(item)).shift()
-    if (!initTabHeader) return
-    const index = tabHeaderList.findIndex(item => item === initTabHeader)
-    const key = initTabHeader?.getAttribute('ran-key') || `${index}`
+    const tabHeaderList = [...this._nav.children];
+    const initTabHeader = tabHeaderList
+      .filter((item) => !isDisabled(item))
+      .shift();
+    if (!initTabHeader) return;
+    const index = tabHeaderList.findIndex((item) => item === initTabHeader);
+    const key = initTabHeader?.getAttribute("ran-key") || `${index}`;
     if (this.active === null && key !== null) {
-      this.setAttribute('active', `${key}`)
-      const { width = 0 } = initTabHeader.getBoundingClientRect()
+      this.setAttribute("active", `${key}`);
+      setElementClass(initTabHeader, "active");
+      const { width = 0 } = initTabHeader.getBoundingClientRect();
       this._line.style.setProperty("width", `${width}px`);
-      this.setTabLine(key)
-      this.setTabContent(key)
+      this.setTabLine(key);
+      this.setTabContent(key);
     }
-  }
+  };
   /**
    * @description: 监听slot组件的添加/删除/替换操作，进行tabs初始化
    * @return {*}
@@ -172,11 +219,15 @@ class Tabs extends HTMLElement {
   listenSlotChange = () => {
     const slots = this._slot.assignedElements();
     slots.forEach((item, index) => {
-      const tabPane = this.createTabHeader(item, index)
-      this._nav.appendChild(tabPane)
-      tabPane.addEventListener('click', this.clickTabHead)
+      const tabPane = this.createTabHeader(item, index);
+      this._nav.appendChild(tabPane);
+      tabPane.addEventListener("click", this.clickTabHead);
     });
-    this.initActive()
+    this.initActive();
+    if (this.align) {
+      if (this.align === "center") this.initTabLineAlignCenter();
+      if (this.align === "end") this.initTabLineAlignEnd();
+    }
   };
 
   connectedCallback() {
@@ -189,11 +240,21 @@ class Tabs extends HTMLElement {
 
   attributeChangedCallback(name: string, oldValue: string, newValue: string) {
     if (oldValue !== newValue) {
-      this.dispatchEvent(new CustomEvent('change', {
-        detail: {
-          active: this.active,
-        }
-      }));
+      this.dispatchEvent(
+        new CustomEvent("change", {
+          detail: {
+            active: this.active,
+          },
+        })
+      );
+    }
+    if (name === "align") {
+      if (newValue === "center") {
+        this.initTabLineAlignCenter();
+      }
+      if (newValue === "end") {
+        this.initTabLineAlignEnd();
+      }
     }
   }
 }
