35d4cf38c231b80fae6f54a4eba7a9c9069c5e12
chaxus <81380@163.com>
2022-11-09 17:24:27 +0800

    feat: 调整文档结构

--- packages/ranui/.vitepress/config.ts
+++ packages/ranui/.vitepress/config.ts
@@ -5,25 +5,33 @@ const sidebar = {
       text: "通用",
       children: [
         { text: "Button 按钮", link: "/docs/button/" },
-        { text: "Input 输入框", link: "/docs/input/" },
-        { text: "Image 图片", link: "/docs/image/" },
+
         { text: "Icon 图标", link: "/docs/icon/" },
-        { text: "Tab 标签页", link: "/docs/tab/" },
-        { text: "message 提示", link: "/docs/message/" },
       ],
     },
-    { text: "导航" },
-    { text: "反馈" },
-    { text: "数据录入" },
-    { text: "数据展示" },
-    { text: "布局" },
+    {
+      text: "数据录入",
+      children: [{ text: "Input 输入框", link: "/docs/input/" }],
+    },
+    {
+      text: "数据展示",
+      children: [
+        { text: "Tab 标签页", link: "/docs/tab/" },
+        { text: "Image 图片", link: "/docs/image/" },
+      ],
+    },
+    {
+      text: "反馈",
+      children: [{ text: "message 提示", link: "/docs/message/" }],
+    },
+   
   ],
 };
 const config = {
   title: "ranui",
-  description:'基于web component的组件库',
+  description: "基于web component的组件库",
   markdown: {
-    lineNumbers: true
+    lineNumbers: true,
   },
   themeConfig: {
     sidebar,
