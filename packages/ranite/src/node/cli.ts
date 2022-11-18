d211ea685c6c5dbf1c780c1a6564baf747f74bc4
chaxus <nouo18@163.com>
2022-11-18 23:29:58 +0800

    wip: 调整cli

--- packages/ranite/src/node/cli.ts
+++ packages/ranite/src/node/cli.ts
@@ -1,27 +1,27 @@
 // 构建cli命令行
 import { startDevServer } from "./server";
 
-const argv = process.argv;
-/**
- * @description:  执行ran dev后，process.argv的值
- * 测试 cli~ [
-  '/Users/.nvm/versions/node/v16.14.2/bin/node',
-  '/Users/Documents/code/ran/packages/ranite/bin/ranite',
-  'serve'
-    ]
- */
-const dev = (cmd: string) => {
-    // 如果命令是dev或者是serve，启动服务
-  if (cmd === "dev" || cmd === "serve") {
-    startDevServer();
-  }
-};
+// cli命令对应的方法
+const commandMapMethod = {
+  dev: startDevServer,
+  serve: startDevServer
+}
+
 function parseOptions() {
   const argv = process.argv;
+  /**
+   * @description:  执行ran dev后，process.argv的值
+   * 测试 cli~ [
+    '/Users/.nvm/versions/node/v16.14.2/bin/node',
+    '/Users/Documents/code/ran/packages/ranite/bin/ranite',
+    'serve'
+      ]
+ */
+  // 获取命令行的命令
   for (let i = 0; i < argv.length; i++) {
     const cmd = argv[i];
-    // console.log("cmd--->", argv, cmd);
-    dev(cmd);
+    const method = commandMapMethod[cmd]
+    if (method) method()
   }
 }
 
