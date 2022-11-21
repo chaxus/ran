687753e8d735c2316c5ad8d9e1b715702fdc6483
chaxus <chaxus@users.noreply.github.com>
2022-11-21 15:32:21 +0000

    deploy: c531872ddd9a7d918f1bcba9bd1e8ef0b745cae0

---
+++ assets/ranuts_index.md.fa8f7b2c.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as a}from"./app.d9adc300.js";const m=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"ranuts/index.md","lastUpdated":1669044681000}'),n={name:"ranuts/index.md"};function r(s,o,c,d,i,p){return a(),t("div")}const f=e(n,[["render",r]]);export{m as __pageData,f as default};
