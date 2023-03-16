86c2da4a40c6b2d398ddf710fee6641b002c30af
chaxus <81380@163.com>
2023-03-16 12:33:42 +0800

    feat: 单元测试

--- packages/ranuts/test/node/network/websocket.test.ts
+++ packages/ranuts/test/node/network/websocket.test.ts
@@ -1,21 +1,28 @@
+import { describe, expect, it } from 'vitest'
 import type { Client } from '../../../src/network/websocket'
 import WSS from '../../../src/network/websocket'
 
-const ws = new WSS({ port: 500 })
-ws.on('connect', (cli: Client) => {
-  console.log('一个客户端连接了')
-  ws.broadcast('Hello! every client!')
-  cli.send('Welcome, a user')
-  cli.ping()
-  cli.connectTime = Date.now()
-  console.log('clients:', ws.clients)
-  cli.on('data', (data: Buffer) => {
-    console.log(data.toString())
+// const ws = new WSS({ port: 500 })
+// ws.on('connect', (cli: Client) => {
+//   console.log('一个客户端连接了')
+//   ws.broadcast('Hello! every client!')
+//   cli.send('Welcome, a user')
+//   cli.ping()
+//   cli.connectTime = Date.now()
+//   console.log('clients:', ws.clients)
+//   cli.on('data', (data: Buffer) => {
+//     console.log(data.toString())
+//   })
+//   cli.on('close', () => {
+//     console.log('a client closed')
+//   })
+//   cli.on('error', (err: Error) => {
+//     console.log(err.message)
+//   })
+// })
+
+describe('astParser', () => {
+    it('websocket', () => {
+      expect(1).toEqual(1)
+    })
   })
-  cli.on('close', () => {
-    console.log('a client closed')
-  })
-  cli.on('error', (err: Error) => {
-    console.log(err.message)
-  })
-})
