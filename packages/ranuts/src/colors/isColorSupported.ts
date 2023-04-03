4acc5e5dbd3798917c791866dd2333339fb5932c
chaxus <81380@163.com>
2023-04-03 11:54:50 +0800

    feat: 调整colors

---
+++ packages/ranuts/src/colors/isColorSupported.ts
@@ -0,0 +1,13 @@
+import tty from 'node:tty'
+import process from 'node:process'
+
+const isColorSupported =
+  !('NO_COLOR' in process.env || process.argv.includes('--no-color')) &&
+  ('FORCE_COLOR' in process.env ||
+    process.argv.includes('--color') ||
+    process.platform === 'win32' ||
+    (tty.isatty(1) && process.env.TERM !== 'dumb') ||
+    'CI' in process.env)
+
+
+export default isColorSupported
