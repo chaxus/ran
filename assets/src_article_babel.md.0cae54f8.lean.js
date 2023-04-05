869406d11843dcd758be5273accbec40fcbd7331
chaxus <chaxus@users.noreply.github.com>
2023-04-05 10:55:13 +0000

    deploy: 8575fab1701d4f5818c37ea11c030224c935a4c5

---
+++ assets/src_article_babel.md.0cae54f8.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as a,o as t,a as l}from"./app.14497a51.js";const T=JSON.parse('{"title":"Babel","description":"","frontmatter":{},"headers":[],"relativePath":"src/article/babel.md","lastUpdated":1680692044000}'),r={name:"src/article/babel.md"},i=l("",3),s=[i];function b(c,o,p,n,_,d){return t(),a("div",null,s)}const h=e(r,[["render",b]]);export{T as __pageData,h as default};
