24232dbeecea50d703e10c9fba92aa8b1d0aab55
chaxus <81380@163.com>
2023-04-17 15:02:49 +0800

    feat: add comment

--- packages/ranuts/src/server/router.ts
+++ packages/ranuts/src/server/router.ts
@@ -24,8 +24,6 @@ import type { Context, MiddlewareFunction, Next } from '@/server/server'
 
 type Handler = (ctx: Context, next: Next) => void
 
-const noop = () => {}
-
 class Router {
   ctx?: Context
   map: Map<string, Map<string, Handler>>
@@ -33,63 +31,39 @@ class Router {
     this.map = new Map()
   }
   get(url: string, handler: Handler): void {
-    let path = this.map.get('GET')
-    if (!path) {
-      path = new Map()
-      this.map.set('GET', path)
-    }
-    path.set(url, handler)
+    this.addHandlerToMap('GET')(url, handler)
   }
   post(url: string, handler: Handler): void {
-    let path = this.map.get('POST')
-    if (!path) {
-      path = new Map()
-      this.map.set('POST', path)
-    }
-    path.set(url, handler)
+    this.addHandlerToMap('POST')(url, handler)
   }
   put(url: string, handler: Handler): void {
-    let path = this.map.get('PUT')
-    if (!path) {
-      path = new Map()
-      this.map.set('PUT', path)
-    }
-    path.set(url, handler)
+    this.addHandlerToMap('PUT')(url, handler)
   }
   patch(url: string, handler: Handler): void {
-    let path = this.map.get('PATCH')
-    if (!path) {
-      path = new Map()
-      this.map.set('PATCH', path)
-    }
-    path.set(url, handler)
+    this.addHandlerToMap('PATCH')(url, handler)
   }
   del(url: string, handler: Handler): void {
-    let path = this.map.get('DELETE')
-    if (!path) {
-      path = new Map()
-      this.map.set('DELETE', path)
-    }
-    path.set(url, handler)
+    this.addHandlerToMap('DELETE')(url, handler)
   }
   head(url: string, handler: Handler): void {
-    let path = this.map.get('HEAD')
-    if (!path) {
-      path = new Map()
-      this.map.set('HEAD', path)
-    }
-    path.set(url, handler)
+    this.addHandlerToMap('HEAD')(url, handler)
   }
   options(url: string, handler: Handler): void {
-    let path = this.map.get('OPTIONS')
-    if (!path) {
-      path = new Map()
-      this.map.set('OPTIONS', path)
+    this.addHandlerToMap('OPTIONS')(url, handler)
+  }
+  private addHandlerToMap(method: string) {
+    return (url: string, handler: Handler): void => {
+      let path = this.map.get(method)
+      if (!path) {
+        path = new Map()
+        this.map.set(method, path)
+      }
+      path.set(url, handler)
     }
-    path.set(url, handler)
   }
   routes(): MiddlewareFunction {
     return (ctx, next) => {
+      this.ctx = ctx
       const { path, method } = ctx.request
       const pathMap = this.map.get(method)
       const handler = pathMap ? pathMap.get(path) : next
@@ -97,7 +71,7 @@ class Router {
     }
   }
   allowedMethods(): MiddlewareFunction {
-    return () => {}
+    return (ctx, next) => {}
   }
 }
 
