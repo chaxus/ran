22e77a3a1989109456062665af3e71158746dc8e
chaxus <nouo18@163.com>
2023-08-22 20:01:41 +0800

    fix: update version

--- packages/ranui/vite.config.ts
+++ packages/ranui/vite.config.ts
@@ -2,13 +2,16 @@ import path, { resolve } from 'node:path'
 import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
+// import viteCompression from 'vite-plugin-compression';
 import loadStyle from './plugins/load-style'
-import autoImportFile from './plugins/auto-import-file'
+// import autoImportFile from './plugins/auto-import-file'
 import loadSvg from './plugins/load-svg'
 
 const __filename = fileURLToPath(import.meta.url)
 
 const __dirname = path.dirname(__filename)
+
+const OUTPUT_FILE_NAME = 'index'
 
 export default defineConfig({
   build: {
@@ -16,7 +19,7 @@ export default defineConfig({
     lib: {
       entry: resolve(__dirname, 'index.ts'),
       name: 'ranui',
-      fileName: 'index',
+      fileName: OUTPUT_FILE_NAME,
       formats: ['es', 'umd'],
     },
   },
@@ -35,6 +38,12 @@ export default defineConfig({
     //   extensions: ['.ts'],
     //   ignore: ['./components/form/index.ts', './components/modal/index.ts', './components/video/index.ts'],
     // }),
+    // viteCompression({
+    //   deleteOriginFile: true,
+    //   threshold: 10240, // 大于10k
+    //   // filter: /^index.js$|^index.umd.js$|.+\.d\.ts$/, // 排除入口文件(index.js,index.umd.cjs)和类型文件
+    //   // filter: /(^index-.+\.js|json|css|html)$/ // 指定需要压缩的文件类型
+    // })
   ],
   resolve: {
     alias: {
