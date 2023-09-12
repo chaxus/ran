4cceec73e0f5481ea95acc5ce91ea41995068be7
chaxus <nouo18@163.com>
2023-09-12 09:15:34 +0800

    feat: update function

---
+++ packages/ranuts/src/optimize/index.ts
@@ -0,0 +1,39 @@
+interface FT {
+    new(): Object,
+    (): void;
+    prototype: Object;
+}
+/**
+ * @description: 实现object.create，将传入的对象作为原型
+ * @param {*} obj
+ * @return {*}
+ */
+const objectCreate = (obj: Object) => {
+    function F() { }
+    F.prototype = obj
+    return new (F as FT)()
+}
+/**
+ * @description: 实现 instanceof: object instanceof constructor
+ * @param {*} left
+ * @param {*} right
+ * @return {*}
+ */
+const instanceOf = (obj: Object, cst: Function) => {
+    let proto = Object.getPrototypeOf(obj) // 获取对象的 prototype
+    const prototype = cst.prototype // 获取构造函数的 prototype
+    while (true) {
+        if (!proto) return false
+        if (proto === prototype) return true
+        proto = Object.getPrototypeOf(proto) // 一直向上获取 prototype
+    }
+}
+/**
+ * @description: 实现new操作符
+ * @param {*} 构造函数
+ * @param {*} 参数
+ * @return {*} Object
+ */
+function customNew() {
+    
+}
