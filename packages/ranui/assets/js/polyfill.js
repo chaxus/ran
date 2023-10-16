6672e2a5d6886d2573be7f53261b6a6d56145849
chaxus <nouo18@163.com>
2023-10-16 22:58:06 +0800

    feat: add lucky license

---
+++ packages/ranui/assets/js/polyfill.js
@@ -0,0 +1,113 @@
+/**
+ * Copy From https://github.com/buuing/lucky-canvas/blob/master/packages/core/src/utils/polyfill.js
+ * Apache-2.0 license: packages/ranui/licenses/lucky-canvas/LICENSE
+ * Change to Adaptive ranui
+ * */
+/**
+ * 由于部分低版本下的某些 app 可能会缺少某些原型方法, 这里增加兼容
+ */
+// ie11 不兼容 includes 方法
+if (!Array.prototype.includes) {
+    Object.defineProperty(Array.prototype, 'includes', {
+      value: function(valueToFind, fromIndex) {
+  
+        if (this == null) {
+          throw new TypeError('"this" is null or not defined');
+        }
+  
+        // 1. Let O be ? ToObject(this value).
+        var o = Object(this);
+  
+        // 2. Let len be ? ToLength(? Get(O, "length")).
+        var len = o.length >>> 0;
+  
+        // 3. If len is 0, return false.
+        if (len === 0) {
+          return false;
+        }
+  
+        // 4. Let n be ? ToInteger(fromIndex).
+        //    (If fromIndex is undefined, this step produces the value 0.)
+        var n = fromIndex | 0;
+  
+        // 5. If n ≥ 0, then
+        //  a. Let k be n.
+        // 6. Else n < 0,
+        //  a. Let k be len + n.
+        //  b. If k < 0, let k be 0.
+        var k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
+  
+        function sameValueZero(x, y) {
+          return x === y || (typeof x === 'number' && typeof y === 'number' && isNaN(x) && isNaN(y));
+        }
+  
+        // 7. Repeat, while k < len
+        while (k < len) {
+          // a. Let elementK be the result of ? Get(O, ! ToString(k)).
+          // b. If SameValueZero(valueToFind, elementK) is true, return true.
+          if (sameValueZero(o[k], valueToFind)) {
+            return true;
+          }
+          // c. Increase k by 1.
+          k++;
+        }
+  
+        // 8. Return false
+        return false;
+      }
+    });
+  }
+  
+  // vivo x7 下网易云游戏 app 缺少 includes 方法
+  if (!String.prototype.includes) {
+    String.prototype.includes = function(search, start) {
+      'use strict';
+      if (typeof start !== 'number') {
+        start = 0;
+      }
+      if (start + search.length > this.length) {
+        return false;
+      } else {
+        return this.indexOf(search, start) !== -1;
+      }
+    };
+  }
+  
+  // vivo x7 下网易云游戏 app 缺少 find 方法
+  if (!Array.prototype.find) {
+    Object.defineProperty(Array.prototype, 'find', {
+      value: function(predicate) {
+       // 1. Let O be ? ToObject(this value).
+        if (this == null) {
+          throw new TypeError('"this" is null or not defined');
+        }
+        var o = Object(this);
+        // 2. Let len be ? ToLength(? Get(O, "length")).
+        var len = o.length >>> 0;
+        // 3. If IsCallable(predicate) is false, throw a TypeError exception.
+        if (typeof predicate !== 'function') {
+          throw new TypeError('predicate must be a function');
+        }
+        // 4. If thisArg was supplied, let T be thisArg; else let T be undefined.
+        var thisArg = arguments[1];
+        // 5. Let k be 0.
+        var k = 0;
+        // 6. Repeat, while k < len
+        while (k < len) {
+          // a. Let Pk be ! ToString(k).
+          // b. Let kValue be ? Get(O, Pk).
+          // c. Let testResult be ToBoolean(? Call(predicate, T, « kValue, k, O »)).
+          // d. If testResult is true, return kValue.
+          var kValue = o[k];
+          if (predicate.call(thisArg, kValue, k, o)) {
+            return kValue;
+          }
+          // e. Increase k by 1.
+          k++;
+        }
+        // 7. Return undefined.
+        return void 0;
+      }
+    });
+  }
+  
