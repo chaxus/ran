92dba4c78b8738710cfa864f2a72bc9bd7877757
chaxus <nouo18@163.com>
2023-12-01 20:03:03 +0800

    Feat/uts (#190)

--- packages/ranuts/src/utils/index.ts
+++ packages/ranuts/src/utils/index.ts
@@ -731,3 +731,92 @@ export const createDocumentFragment = (
   list.forEach((item) => Fragment.appendChild(item));
   return Fragment;
 };
+
+interface QuestQueueOptions {
+  simultaneous: number;
+  total: number;
+}
+
+type Fun = Function | Func;
+
+export class QuestQueue {
+  current: number;
+  queue: Fun[];
+  simultaneous: number; // 并发的请求
+  executed: number;
+  total: number;
+  constructor({ simultaneous, total }: QuestQueueOptions) {
+    this.current = 0; // 当前请求有几个
+    this.queue = []; // 队列
+    this.simultaneous = simultaneous; // 并发的请求数量
+    this.executed = 0; // 执行了几个请求
+    this.total = total; // 总共有几个请求
+  }
+  /**
+   * @description: 传入异步函数，添加到队列并执行
+   * @param {Fun} asynchronous
+   * @return {*}
+   */  
+  add = (asynchronous: Fun): void => {
+    if (typeof asynchronous !== 'function') return;
+    const task = () => {
+      return new Promise((resolve, reject) => {
+        this.current++;
+        asynchronous()
+          .then(resolve)
+          .catch(reject)
+          .finally(() => {
+            this.current--;
+            this.running();
+          });
+      });
+    };
+    this.queue.push(task);
+  };
+  /**
+   * @description: 执行异步函数
+   * @param {*} Promise
+   * @return {*}
+   */  
+  running = (): Promise<unknown> => {
+    return new Promise((resolve, reject) => {
+      if (this.current <= this.simultaneous && this.queue.length) {
+        const task = this.queue.pop();
+        if (task) {
+          task()
+            .then(resolve)
+            .catch(reject)
+            .finally(() => {
+              this.executed++;
+            });
+        }
+      } else {
+        resolve({});
+      }
+    });
+  };
+  /**
+   * @description: 并发执行所有的异步函数，并返回所有的结果
+   * @param {*} Promise
+   * @return {*}
+   */  
+  allSettled = (): Promise<unknown> => {
+    return new Promise((resolve, reject) => {
+      const result: unknown[] = [];
+      if (this.current < this.simultaneous && this.queue.length) {
+        const task = this.queue.pop();
+        if (task) {
+          task()
+            .then(resolve)
+            .catch(reject)
+            .finally(() => {
+              this.executed++;
+            });
+        }
+      }
+      if (this.executed >= this.total) {
+        resolve(result);
+      }
+    });
+  };
+}
