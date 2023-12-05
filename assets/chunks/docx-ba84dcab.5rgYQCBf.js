8a55b1433f507c93a72ac6918257094eca1d3621
chaxus <chaxus@users.noreply.github.com>
2023-12-05 15:43:00 +0000

    deploy: 4168d17f9ed7b913124db952ce2d8f60dac67bf8

---
+++ assets/chunks/docx-ba84dcab.5rgYQCBf.js
@@ -0,0 +1,1 @@
+import{d}from"./index.rN1gS48v.js";import"./framework.QGErRw_c.js";const p=i=>{if(typeof window<"u"){const{bodyContainer:n,styleContainer:e,buffer:o,docxOptions:c={}}=i,t=Object.assign({},{className:"docx"},c);if(n)return d.renderAsync(o,n,e,t);{const r=document.createElement("div");return document.body.appendChild(r),d.renderAsync(o,r,e,t)}}};export{p as renderDocx};
