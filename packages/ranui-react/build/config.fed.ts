197e2a8dfb6586b1a7a3b6038efd384a39e7f4ef
chaxus <nouo18@163.com>
2024-01-09 00:09:27 +0800

    feat: update ranui-react federation

--- packages/ranui-react/build/config.fed.ts
+++ packages/ranui-react/build/config.fed.ts
@@ -14,6 +14,16 @@ viteConfig.plugins?.push(federation({
         './button': './components/button/index.ts',
         './input': './components/input/index.ts',
         './player': './components/player/index.ts',
+        './icon': './components/icon/index.ts',
+        './image': './components/image/index.ts',
+        './option': './components/option/index.ts',
+        './preview': './components/preview/index.ts',
+        './progress': './components/progress/index.ts',
+        './radar': './components/radar/index.ts',
+        './select': './components/select/index.ts',
+        './skeleton': './components/skeleton/index.ts',
+        './tab': './components/tab/index.ts',
+        './tabpane': './components/tabpane/index.ts',
     },
 }))
 
