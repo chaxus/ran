10d4e89d6d6306c502b3cefc2832338dd13dc255
chaxus <nouo18@163.com>
2024-03-28 10:06:07 +0800

    chore: test docs build.sh (#223)

--- packages/docs/bin/build.sh
+++ packages/docs/bin/build.sh
@@ -1,50 +1,45 @@
-
+#!/bin/bash 
 
 bin=./node_modules/.bin
 
 $bin/vitepress build
-
 # 指定目录  
-dir=".vitepress/dist"  
+dir=".vitepress/dist"
 # 临时缓存文件
 output=".vitepress/dist/sw-file.js"
 # 生成的目标文件
 target=".vitepress/dist/sw.js"
-
 # 使用find命令将文件路径输出到一个临时文件  
-tmpfile=$(mktemp)  
-find "$dir" -type f > "$tmpfile"  
-  
+tmpfile=$(mktemp)
+find "$dir" -type f > "$tmpfile"
 # 从临时文件中读取文件路径并存储到数组中  
-while IFS= read -r file; do  
+while IFS= read -r file; do
     file_paths+=("$file")
-done < "$tmpfile"  
-  
-# 删除临时文件  
-rm "$tmpfile"  
-
-echo "const serviceWorkCacheFilePaths = [" > "$output"  
-  
+done < "$tmpfile"
+# 删除临时文件
+rm "$tmpfile"
+# 拼接字符串
+echo "const serviceWorkCacheFilePaths = [" > "$output"
+# 根路径
 ran="/ran"
 # 打印数组中的文件路径  
 for path in "${file_paths[@]}"; do
-    if [[ $path != *".DS_Store"* ]]; then  
-        str="${path##.vitepress/dist}"  
-        echo "\"$ran$str\"," >> "$output"  
+    if [[ $path != *".DS_Store"* ]]; then
+        str="${path##.vitepress/dist}"
+        echo "\"$ran$str\"," >> "$output"
     fi
 done
-
+# 拼接字符串
 echo "];" >> "$output"
 
-tmpfile=$(mktemp)  
+tmpfile=$(mktemp)
 
-cat "$output" >> "$tmpfile"  
+cat "$output" >> "$tmpfile"
 
 cat "$target" >> "$tmpfile"
 
-mv "$tmpfile" "$target" 
+mv "$tmpfile" "$target"
 
-rm "$output"  
-
+rm "$output"
 # 打印完成消息  
 echo "service work file paths have been generate for $target"
