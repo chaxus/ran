9df96e900b49a9a1f9b69ca9c6cb80a2a174ca67
ranzhouhang <zhouhang.ran@longbridge-inc.com>
2024-04-23 09:40:40 +0800

    feat: add author,robots,renderer,google meta to document

--- packages/docs/.vitepress/config.ts
+++ packages/docs/.vitepress/config.ts
@@ -85,6 +85,18 @@ export default defineConfig({
     ['link', { rel: 'icon', href: `${BASE_PATH}favicon.ico` }],
     ['link', { rel: 'manifest', href: `${BASE_PATH}manifest.json` }],
     ['meta', { name: 'theme-color', content: '#646cff' }],
+    // author
+    ['meta', { name: 'author', content: '81380@163.com' }],
+    // 表示爬虫对此页面的处理行为 或 应当遵守的规则，是用来做搜索引擎抓取的
+    // all：搜索引擎将索引此网页，并继续通过此 网页的链接索引文件 将被检索
+    // none：搜索引擎将 忽略 此网页
+    // index：搜索引擎 索引 此网页
+    // follow：搜索引擎继续通过此网页的链接索引搜索 其它的网页
+    ['meta', { name: 'robots', content: 'all' }],
+    // 用来指定支持双核浏览器要采用哪种的渲染方式
+    ['meta', { name: 'renderer', content: 'webkit' }],
+    // 已经有国际化，禁止谷歌自动翻译
+    ['meta', { name: 'google', content: 'notranslate' }],
     // og
     ['meta', { property: 'og:title', content: 'ran' }],
     [
@@ -101,10 +113,11 @@ export default defineConfig({
       'meta',
       {
         property: 'article:home',
-        content: UTILS_PATH,
+        content: HOME,
       },
     ],
     ['meta', { property: 'article:ranui', content: RANUI_PATH }],
+    ['meta', { property: 'article:ranuts', content: UTILS_PATH }],
     ['meta', { property: 'article:section', content: ARTICLE_PATH }],
     // keywords
     ['meta', { name: 'keywords', content: KEY_WORDS }],
@@ -118,6 +131,7 @@ export default defineConfig({
     ['script', {}, BD_ANALYSE],
     // preview component script
     ['script', {}, PREVIEW_CODE],
+    // service worker and pwa
     ['script', {}, SERVICE_WORK],
   ],
   themeConfig: themeEnConfig,
