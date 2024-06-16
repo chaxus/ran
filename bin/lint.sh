a1cddde72e497672c0f45d515c1278172fb983fc
chaxus <nouo18@163.com>
2024-06-16 16:05:39 +0800

    fix: ci fail

--- bin/lint.sh
+++ bin/lint.sh
@@ -1,4 +1,4 @@
 
-# bin=./node_modules/.bin
-# $bin/eslint --cache . && $bin/prettier --check --cache .
-npm run lint:es && npm run lint:prettier
+bin=./node_modules/.bin
+$bin/eslint --cache . && $bin/prettier --check --cache .
+# npm run lint:es && npm run lint:prettier
