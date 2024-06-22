51fce43274a90b5fdcb192cd555f0bbe892619e9
chaxus <nouo18@163.com>
2024-06-22 13:36:24 +0800

    fix: react types

--- packages/ranui/types/react.ts
+++ packages/ranui/types/react.ts
@@ -1,7 +1,8 @@
-import { NAME_AMP } from '@/components/loading';
 import type React from 'react';
+import type { NAME_AMP } from '@/components/loading';
 
 declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace JSX {
     interface IntrinsicElements {
       'r-loading': React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement> & {
