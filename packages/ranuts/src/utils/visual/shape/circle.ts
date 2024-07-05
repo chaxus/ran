a39ee2a8fd647a87abeac81c8a8bf4dd06373e4b
ranzhouhang <zhouhang.ran@longbridge-inc.com>
2024-07-05 20:32:32 +0800

    fix: 给正数前面加一个正号，其他情况不变

---
+++ packages/ranuts/src/utils/visual/shape/circle.ts
@@ -0,0 +1,20 @@
+import { Shape } from '@/utils/visual/shape/shape'
+import { ShapeType } from '@/utils/visual/enums'
+import { Point } from '@/utils/visual/point'
+
+
+export class Circle extends Shape {
+  public x: number
+  public y: number
+  public radius: number
+  public readonly type = ShapeType.Circle
+  constructor(x = 0, y = 0, radius = 0) {
+    super()
+    this.x = x
+    this.y = y
+    this.radius = radius
+  }
+  public contains(point: Point): boolean {
+    return true
+  }
+}
