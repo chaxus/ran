b6a49b8cdac22a245a16cc93adffd27c3ac10f30
ranzhouhang <zhouhang.ran@longbridge-inc.com>
2024-07-05 09:45:56 +0800

    feat: add comment to subscribe

--- packages/ranuts/src/utils/subscribe.ts
+++ packages/ranuts/src/utils/subscribe.ts
@@ -15,6 +15,12 @@ export class SyncHook {
   constructor() {
     this._events = {};
   }
+  /**
+   * @description: 订阅事件
+   * @param {EventName} eventName
+   * @param {EventItem} eventItem
+   * @return {void}
+   */
   tap = (eventName: EventName, eventItem: EventItem | Callback): void => {
     if (this._events[eventName] && eventName !== Symbol.for(NEW_LISTENER)) {
       // 注册一个 newListener 用于监听新的事件订阅
@@ -34,7 +40,12 @@ export class SyncHook {
 
     this._events[eventName] = callbacks;
   };
-
+  /**
+   * @description: 触发事件
+   * @param {EventName} eventName
+   * @param {array} args
+   * @return {void}
+   */
   call = (eventName: EventName, ...args: Array<unknown>): void => {
     const callbacks = this._events[eventName] || [];
     callbacks.forEach((item) => {
@@ -42,7 +53,12 @@ export class SyncHook {
       callback(...args);
     });
   };
-
+  /**
+   * @description: 同步触发事件
+   * @param {EventName} eventName
+   * @param {array} args
+   * @return {Promise<void>}
+   */
   callSync = async (eventName: EventName, ...args: Array<unknown>): Promise<void> => {
     const callbacks = this._events[eventName] || [];
     for (const item of callbacks) {
@@ -50,7 +66,12 @@ export class SyncHook {
       await callback(...args);
     }
   };
-
+  /**
+   * @description: 只订阅一次事件，触发后就移除事件
+   * @param {EventName} eventName
+   * @param {EventItem} eventItem
+   * @return {void}
+   */
   once = (eventName: EventName, eventItem: EventItem | Callback): void => {
     let one: EventItem;
     if (typeof eventItem === 'function') {
@@ -73,14 +94,18 @@ export class SyncHook {
         initialCallback: callback,
       };
     }
-    // 由于需要在回调函数执行后，取消订阅当前事件，所以需要对传入的回调函数做一层包装,然后绑定包装后的函数
-
+    // 由于需要在回调函数执行后，取消订阅当前事件，所以需要对传入的回调函数做一层包装，然后绑定包装后的函数
     // 考虑：如果当前事件在未执行，被用户取消订阅，能否取消？
     // 由于：我们订阅事件的时候，修改了原回调函数的引用，所以，用户触发 off 的时候不能找到对应的回调函数
     // 所以，我们需要在当前函数与用户传入的回调函数做一个绑定，我们通过自定义属性来实现
     this.tap(eventName, one);
   };
-
+  /**
+   * @description: 移除订阅的事件
+   * @param {EventName} eventName
+   * @param {EventItem} eventItem
+   * @return {void}
+   */
   off = (eventName: EventName, eventItem: EventItem | Callback): void => {
     // 找到事件对应的回调函数，删除对应的回调函数
     const callbacks = this._events[eventName] || [];
@@ -89,7 +114,7 @@ export class SyncHook {
         return item.callback !== eventItem && item.initialCallback !== eventItem;
       } else {
         const { callback } = eventItem;
-        /* 用于once的取消订阅 */
+        /* 用于 once 的取消订阅 */
         return item.callback !== callback && item.initialCallback !== callback;
       }
     });
@@ -103,9 +128,9 @@ export class SyncHook {
 
 // SyncLoopHook 是一个同步、循环类型的 Hook。循环类型的含义是不停的循环执行事件函数，直到所有函数结果 result === undefined，不符合条件就调头重新开始执行。
 
-// 异步的hook
-// 异步钩子需要通过tapAsync函数注册事件,同时也会多一个callback参数，执行callback告诉hook该注册事件已经执行完成
-// call方法只有同步钩子才有，异步钩子得使用callAsync
+// 异步的 hook
+// 异步钩子需要通过 tapAsync 函数注册事件，同时也会多一个 callback 参数，执行 callback 告诉 hook 该注册事件已经执行完成
+// call 方法只有同步钩子才有，异步钩子得使用 callAsync
 
 // AsyncParallelHook 是一个异步并行、基本类型的 Hook，它与同步 Hook 不同的地方在于：
 // 它会同时开启多个异步任务，而且需要通过 tapAsync 方法来注册事件（同步 Hook 是通过 tap 方法）
