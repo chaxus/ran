108931aa77d0d26415c62ad284e7e89d37dbb1cd
chaxus <nouo18@163.com>
2024-07-20 22:21:52 +0800

    feat: ranui packagejson add dist path

--- packages/ranui/bin/build.sh
+++ packages/ranui/bin/build.sh
@@ -19,12 +19,12 @@ do
 done
 
 # build es and splite bundle
-$bin/tsc --declaration --emitDeclarationOnly --outDir ./dist --project tsconfig.json
 $bin/vite build -c ./build/config.es.ts
 # build umd
 # TODO: splite bundle
 $bin/vite build -c ./build/config.umd.ts
 $bin/vite build -c ./build/config.sha.ts
+$bin/tsc --declaration --emitDeclarationOnly --outDir ./dist --project tsconfig.json
 
 cp tsconfig.json ./dist
 cp typings.d.ts ./dist
