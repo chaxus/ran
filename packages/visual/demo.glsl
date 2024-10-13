489887c82019cd8d84ca9fd13a35136c7697f1d7
chaxus <nouo18@163.com>
2024-10-13 21:25:12 +0800

    docs: add solidity description

--- packages/visual/demo.glsl
+++ packages/visual/demo.glsl
@@ -1,13 +1,18 @@
-#iChannel0 "https://s2.loli.net/2023/09/10/QozT59R6KsYmb3q.jpg"
-
-vec2 distort(vec2 p){
-    p.x+=sin(p.y*10.+iTime)/50.;
-    return p;
+highp float random(vec2 co)
+{
+    highp float a=12.9898;
+    highp float b=78.233;
+    highp float c=43758.5453;
+    highp float dt=dot(co.xy,vec2(a,b));
+    highp float sn=mod(dt,3.14);
+    return fract(sin(sn)*c);
 }
 
 void mainImage(out vec4 fragColor,in vec2 fragCoord){
     vec2 uv=fragCoord/iResolution.xy;
-    uv=distort(uv);
-    vec3 tex=texture(iChannel0,uv).xyz;
-    fragColor=vec4(tex,1.);
+
+    float noise=random(uv);
+
+    vec3 col=vec3(noise);
+    fragColor=vec4(col,1.);
 }
---
+++ packages/visual/index.md
@@ -0,0 +1,13 @@
+## GLSL 语法
+
+glsl.md
+
+## UX
+
+ux.md
+
+## 纹理
+
+texture.md
+
+## 随机与噪声
