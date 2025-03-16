271bf2cc7aae0a02871c7f429964964623dec6f2
chaxus <nouo18@163.com>
2025-03-16 17:53:40 +0800

    feat: add catalogue component

---
+++ packages/read/components/Catalogue/index.tsx
@@ -0,0 +1,101 @@
+import { useCallback, useEffect, useRef, useState } from 'react';
+import type { BookInfo } from '@/store/books';
+import type { TextSyntaxTree } from '@/lib/transformText';
+import { EVENT_NAME, synchook } from '@/lib/subscribe';
+
+export enum SORT_DIRECTION {
+    UP = 'UP',
+    DOWN = 'DOWN',
+}
+
+const SORT_ICON_STYLE = {
+    '--ran-icon-font-size': '20px',
+};
+
+export interface CatalogueProps {
+    bookDetail?: BookInfo,
+    textSyntaxTree: TextSyntaxTree,
+    setPageNum?: (page: number) => void
+}
+
+export const Catalogue = ({ bookDetail, textSyntaxTree, setPageNum }: CatalogueProps): React.JSX.Element => {
+    const { title, author, image } = bookDetail || {};
+    const { titleIdTitle = [], titleIdPage } = textSyntaxTree || {};
+    const sortRef = useRef<HTMLDivElement>(null);
+    const scrollRef = useRef<HTMLDivElement>(null);
+    const [sortDirection, setSortDirection] = useState(SORT_DIRECTION.DOWN);
+
+    const toPage = (e: Event) => {
+        const index = (e.target as HTMLElement)?.getAttribute('title') || '';
+        if (index === undefined) return;
+        const page = titleIdPage[index];
+        if (setPageNum && page !== undefined) {
+            // Fallback for browsers that don't support View Transitions API
+            if (!document.startViewTransition) {
+                setPageNum(page);
+                return;
+            }
+            // With View Transition
+            document.startViewTransition(() => {
+                setPageNum(page);
+            });
+        }
+        synchook.call(EVENT_NAME.CLOSE_POPOVER);
+    }
+
+    const toSort = useCallback(() => {
+        setSortDirection(SORT_DIRECTION.UP);
+        if (sortDirection === SORT_DIRECTION.DOWN) {
+            const bottom = scrollRef.current?.scrollHeight;
+            scrollRef.current?.scrollTo({ top: bottom, behavior: 'smooth' });
+            setSortDirection(SORT_DIRECTION.UP);
+        }
+        if (sortDirection === SORT_DIRECTION.UP) {
+            scrollRef.current?.scrollTo({ top: 0, behavior: 'smooth' });
+            setSortDirection(SORT_DIRECTION.DOWN);
+        }
+    }, [sortDirection])
+
+    useEffect(() => {
+        scrollRef.current?.addEventListener('click', toPage);
+        sortRef.current?.addEventListener('click', toSort);
+        return () => {
+            scrollRef.current?.removeEventListener('click', toPage);
+            sortRef.current?.removeEventListener('click', toSort);
+        };
+    }, [textSyntaxTree, bookDetail, sortDirection]);
+
+    return (
+        <>
+            <div className="px-7 py-2 flex flex-row flex-nowrap items-center shrink-0">
+                <img className="w-14 mr-5" src={image} />
+                <div>
+                    <div className="text-lg text-text-color-1 font-medium break-all">{title}</div>
+                    <div className="text-sm text-text-color-2 font-medium mt-1 break-all">{author}</div>
+                </div>
+            </div>
+            <div className="mx-9 basis-10 flex items-center justify-end shrink-0" ref={sortRef}>
+                <r-icon
+                    className={`cursor-pointer hover-icon rotate-180 ${sortDirection}`}
+                    name="sort"
+                    style={SORT_ICON_STYLE}
+                ></r-icon>
+            </div>
+            <div className="overflow-y-auto flex-auto" ref={scrollRef}>
+                {titleIdTitle?.map((item, index) => {
+                    return (
+                        <div
+                            className="px-7 h-12 text-text-color-2 font-normal text-base hover:bg-blue-50 cursor-pointer"
+                            title={`${index}`}
+                            key={index}
+                        >
+                            <div className="border-t border-front-bg-color-1 h-full w-full flex items-center" title={`${index}`}>
+                                {item}
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+        </>
+    );
+};
--- packages/read/components/DetailMenu/index.tsx
+++ packages/read/components/DetailMenu/index.tsx
@@ -1,125 +1,95 @@
 import { useCallback, useEffect, useRef, useState } from 'react';
-import { EVENT_NAME, synchook } from '@/lib/subscribe';
+import { debounce } from 'ranuts/utils';
+import { trim } from '@/lib/transformText';
 import type { BookInfo } from '@/store/books';
-import type { TextSyntaxTree } from '@/lib/transformText';
+import type { PagingTextItem, TextSyntaxTree } from '@/lib/transformText';
+import { Catalogue } from '@/components/Catalogue';
 
 export interface MenuProps {
-  bookDetail?: BookInfo;
-  setPageNum?: (num: number) => void;
-  textSyntaxTree: TextSyntaxTree;
+    bookDetail?: BookInfo;
+    setPageNum?: (num: number) => void;
+    textSyntaxTree: TextSyntaxTree;
 }
 
-const SORT_ICON_STYLE = {
-  '--ran-icon-font-size': '20px',
-};
-
 const inputStyle = {
-  '--ran-input-border-radius': '2rem',
-  '--ran-input-content-border-radius': '2rem',
-  '--ran-input-content-padding': '10px',
-  '--ran-input-content-font-size': '14px',
-  '--ran-input-content-font-weight': '400',
-  '--ran-icon-font-size': '16px',
-  '--ran-icon-color': 'var(--icon-color-1)',
-  '--ran-icon-margin': '2px 0px 0px 12px',
-  '--ran-input-background-color': 'rgba(13,20,30,.04)',
-  '--ran-input-content-background-color': 'transparent',
-  '--ran-input-border': 'none',
+    '--ran-input-border-radius': '2rem',
+    '--ran-input-content-border-radius': '2rem',
+    '--ran-input-content-padding': '10px',
+    '--ran-input-content-font-size': '14px',
+    '--ran-input-content-font-weight': '400',
+    '--ran-icon-font-size': '16px',
+    '--ran-icon-color': 'var(--icon-color-1)',
+    '--ran-icon-margin': '2px 0px 0px 12px',
+    '--ran-input-background-color': 'rgba(13,20,30,.04)',
+    '--ran-input-content-background-color': 'transparent',
+    '--ran-input-border': 'none',
 };
 
 export enum SORT_DIRECTION {
-  UP = 'UP',
-  DOWN = 'DOWN',
+    UP = 'UP',
+    DOWN = 'DOWN',
 }
 
 export const BookDetailMenu = ({ bookDetail, setPageNum, textSyntaxTree }: MenuProps): React.JSX.Element => {
-  const { title, author, image } = bookDetail || {};
-  const { titleIdTitle = [], titleIdPage = {} } = textSyntaxTree || {};
-  const scrollRef = useRef<HTMLDivElement>(null);
-  const sortRef = useRef<HTMLDivElement>(null);
-  const [sortDirection, setSortDirection] = useState(SORT_DIRECTION.DOWN);
+    const searchRef = useRef<HTMLInputElement>(null);
+    const [showSearchResult, setShowSearchResult] = useState(false);
+    const [searchResult, setSearchResult] = useState<{ title: { item: string; index: number }[]; page: { item: PagingTextItem; index: number }[] }>({ title: [], page: [] });
+    
+    const onSearch = useCallback(
+        debounce((e: Event) => {
+            const value = (e.target as HTMLInputElement)?.value || '';
+            const searchValue = trim(value);
+            // 没有输入内容，则不进行搜索
+            if (!searchValue) {
+                setShowSearchResult(false);
+                return
+            }
+            setShowSearchResult(true);
+            console.log('value---->', value, textSyntaxTree, searchResult);
+            const { titleIdTitle = [], pageText = [] } = textSyntaxTree || {};
+            const titleSearchResult: { item: string; index: number }[] = []
+            titleIdTitle.forEach((item, index) => {
+                if (item.includes(searchValue)) {
+                    titleSearchResult.push({ item, index });
+                }
+            })
+            const pageSearchResult: { item: PagingTextItem; index: number }[] = []
+            pageText.forEach((item, index) => {
+                if (item.text.includes(searchValue)) {
+                    pageSearchResult.push({ item, index });
+                }
+            })
+            // 没有搜索结果
+            if (titleSearchResult.length <= 0 && pageSearchResult.length <= 0) {
+                return;
+            }
+            setSearchResult({ title: titleSearchResult, page: pageSearchResult });
+        }, 500),
+        [textSyntaxTree],
+    );
 
-  const toPage = useCallback(
-    (e: Event) => {
-      const index = (e.target as HTMLElement)?.getAttribute('title') || '';
-      if (index === undefined) return;
-      const page = titleIdPage[index];
-      if (setPageNum && page !== undefined) {
-        // Fallback for browsers that don't support View Transitions API
-        if (!document.startViewTransition) {
-          setPageNum(page);
-          return;
-        }
-        // With View Transition
-        document.startViewTransition(() => {
-          setPageNum(page);
-        });
-      }
-      synchook.call(EVENT_NAME.CLOSE_POPOVER);
-    },
-    [textSyntaxTree],
-  );
+    useEffect(() => {
+        searchRef.current?.addEventListener('change', onSearch);
+        return () => {
+            searchRef.current?.removeEventListener('change', onSearch);
+        };
+    }, [textSyntaxTree]);
 
-  const toSort = useCallback(() => {
-    if (sortDirection === SORT_DIRECTION.DOWN) {
-      const bottom = scrollRef.current?.scrollHeight;
-      scrollRef.current?.scrollTo({ top: bottom, behavior: 'smooth' });
-      setSortDirection(SORT_DIRECTION.UP);
-    }
-    if (sortDirection === SORT_DIRECTION.UP) {
-      scrollRef.current?.scrollTo({ top: 0, behavior: 'smooth' });
-      setSortDirection(SORT_DIRECTION.DOWN);
-    }
-  }, [sortDirection]);
-
-  useEffect(() => {
-    scrollRef.current?.addEventListener('click', toPage);
-    sortRef.current?.addEventListener('click', toSort);
-    return () => {
-      scrollRef.current?.removeEventListener('click', toPage);
-      sortRef.current?.removeEventListener('click', toSort);
-    };
-  }, [textSyntaxTree, sortDirection]);
-
-  return (
-    <div
-      className="w-md flex flex-col"
-      style={{
-        height: 'calc(100vh - calc(var(--spacing) * 32))',
-      }}
-    >
-      <div className="px-6 py-7">
-        <r-input className="h-10" icon="search" style={inputStyle} placeholder="搜索"></r-input>
-      </div>
-      <div className="px-7 py-2 flex flex-row flex-nowrap items-center shrink-0">
-        <img className="w-14 mr-5" src={image} />
-        <div>
-          <div className="text-lg text-text-color-1 font-medium break-all">{title}</div>
-          <div className="text-sm text-text-color-2 font-medium mt-1 break-all">{author}</div>
-        </div>
-      </div>
-      <div className="mx-9 basis-10 flex items-center justify-end shrink-0" ref={sortRef}>
-        <r-icon
-          className={`cursor-pointer hover-icon rotate-180 ${sortDirection}`}
-          name="sort"
-          style={SORT_ICON_STYLE}
-        ></r-icon>
-      </div>
-      <div className="overflow-y-auto flex-auto" ref={scrollRef}>
-        {titleIdTitle?.map((item, index) => {
-          return (
-            <div
-              className="px-7 h-12 text-text-color-2 font-normal text-base hover:bg-blue-50 cursor-pointer"
-              title={`${index}`}
-              key={index}
-            >
-              <div className="border-t border-front-bg-color-1 h-full w-full flex items-center" title={`${index}`}>
-                {item}
-              </div>
+    return (
+        <div
+            className="w-md flex flex-col"
+            style={{
+                height: 'calc(100vh - calc(var(--spacing) * 32))',
+            }}
+        >
+            <div className="px-6 py-7">
+                <r-input className="h-10" icon="search" style={inputStyle} placeholder="搜索" ref={searchRef}></r-input>
             </div>
-          );
-        })}
-      </div>
-    </div>
-  );
+            {!showSearchResult ? (
+                <Catalogue bookDetail={bookDetail} textSyntaxTree={textSyntaxTree} setPageNum={setPageNum} />
+            ) : (
+                <div></div>
+            )}
+        </div>
+    );
 };
