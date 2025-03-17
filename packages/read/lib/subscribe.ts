d4a5ffabe504e52afb7e77f889c765f0b1fd22d8
chaxus <nouo18@163.com>
2025-03-17 09:30:16 +0800

    feat: add subscript to read store

--- packages/read/lib/subscribe.ts
+++ packages/read/lib/subscribe.ts
@@ -1,9 +1,12 @@
-import { SyncHook } from 'ranuts/utils';
-
-export const synchook = new SyncHook();
+import { createSignal, subscribers } from 'ranuts/utils';
+import type { BookInfo } from '@/store/books';
 
 export enum EVENT_NAME {
   CLOSE_POPOVER = 'close-popover',
   SET_CURRENT_BOOK_PAGE = 'set-current-book-page',
   SET_CURRENT_BOOK_DETAIL = 'set-current-book-detail',
 }
+
+export const syncHook = subscribers
+
+export const [getCurrentBookDetail, setCurrentBookDetail] = createSignal<BookInfo>({ subscriber: EVENT_NAME.SET_CURRENT_BOOK_DETAIL });
