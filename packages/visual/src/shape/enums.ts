a2acd1684ab2e15e379e6d2eab5eaec108ae0a5a
chaxus <nouo18@163.com>
2025-04-04 16:43:02 +0800

    feat: init visual

---
+++ packages/visual/src/shape/enums.ts
@@ -0,0 +1,8 @@
+// 支持的形状类型
+export enum SHAPE_TYPE {
+    RECTANGLE = 'rectangle', // 矩形
+    POLYGON = 'polygon', // 多边形
+    CIRCLE = 'circle', // 圆形
+    ELLIPSE = 'ellipse', // 椭圆
+    ROUNDED_RECTANGLE = 'rounded rectangle', // 圆角矩形
+  }
