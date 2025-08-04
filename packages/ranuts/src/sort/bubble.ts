4e236402b229d0145c019c713069435e211d1278
chaxus <nouo18@163.com>
2025-08-04 23:50:18 +0800

    refactor: simplify bubble sort implementation using array destructuring for swaps

--- packages/ranuts/src/sort/bubble.ts
+++ packages/ranuts/src/sort/bubble.ts
@@ -4,17 +4,16 @@
  * @return {Array}
  */
 const bubble = (list: number[]): number[] => {
-  const size = list.length;
-  for (let i = 0; i < size - 1; i++) {
-    for (let j = 0; j < size - i - 1; j++) {
-      if (list[j] > list[j + 1]) {
-        list[j] = list[j] ^ list[j + 1];
-        list[j + 1] = list[j] ^ list[j + 1];
-        list[j] = list[j] ^ list[j + 1];
-      }
-    }
-  }
-  return list;
+ const size = list.length;
+ for (let i = 0; i < size - 1; i++) {
+   for (let j = 0; j < size - i - 1; j++) {
+     if (list[j] > list[j + 1]) {
+       // 交换位置
+       [list[j], list[j + 1]] = [list[j + 1], list[j]];
+     }
+   }
+ }
+ return list;
 };
 
 export default bubble;
