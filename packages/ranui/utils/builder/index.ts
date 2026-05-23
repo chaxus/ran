34d15fd305cbe0f43ae5ede86460f940ac2b1abe
chaxus <nouo18@163.com>
2026-05-24 00:51:31 +0800

    refactor: update exports in builder index for clarity and structure

--- packages/ranui/utils/builder/index.ts
+++ packages/ranui/utils/builder/index.ts
@@ -1,6 +1,24 @@
-export * from './env';
-export * from './escape';
-export * from './selectors';
-export * from './mocks';
-export * from './core';
-export * from './factory';
+export { isSSR } from './env';
+export { escapeHtml, escapeHtmlAttribute } from './escape';
+export { type MockNode, matchSelector, collectMatches } from './selectors';
+export { DocumentFragmentMock, HTMLElementMock, ShadowRootMock } from './mocks';
+export { type Ref, createRef, ElementBuilder, ShadowBuilder } from './core';
+export {
+  View,
+  Div,
+  Span,
+  Slot,
+  ButtonBuilder,
+  InputBuilder,
+  Style,
+  Label,
+  Ul,
+  Li,
+  Section,
+  Article,
+  Nav,
+  Header,
+  Footer,
+  Main,
+  DeclarativeShadow,
+} from './factory';
